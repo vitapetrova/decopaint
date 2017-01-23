bc693d5576ed7c8c5646550f5a937306e158fd9b
shrewhools <vladyukha@yandex.ru>
2017-01-24 00:29:31 +0300

    add show_products in home page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :category_product
-  enum status: { 'В наличии' => 'in_stock', 'Нет в наличии' => 'not_in_stock', 'custom' => 'Под заказ' }
+  enum status: { 'В наличии' => 'in_stock', 'Нет в наличии' => 'not_in_stock', 'Под заказ' => 'custom' }
 end
