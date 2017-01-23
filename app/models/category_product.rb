cc27912987bd6de61f3b2cf0981dcf332873de36
shrewhools <vladyukha@yandex.ru>
2017-01-23 22:33:41 +0300

    add aws and CrEditor v1

--- app/models/category_product.rb
+++ app/models/category_product.rb
@@ -1,3 +1,12 @@
 class CategoryProduct < ApplicationRecord
   has_many :products
+
+  rails_admin do
+    edit do
+      fields :title, :show_menu
+      field :description, :ck_editor
+      field :description2, :ck_editor
+    end
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category_product
   enum status: { 'В наличии' => 'in_stock', 'Нет в наличии' => 'not_in_stock', 'custom' => 'Под заказ' }
+
+  rails_admin do
+    edit do
+      fields :title, :price, :price2, :status, :min_description
+      field :description, :ck_editor
+      field :additional_description, :ck_editor
+    end
+  end
 end
