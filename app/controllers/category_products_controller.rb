17e079d2714729a7753fb87a3f1a548b045fcb66
shrewhools <vladyukha@yandex.ru>
2017-01-24 01:18:46 +0300

    product page, category_product page code

--- app/controllers/category_products_controller.rb
+++ app/controllers/category_products_controller.rb
@@ -1,4 +1,7 @@
 class CategoryProductsController < ApplicationController
   def show
+    @category_product = CategoryProduct.find(params[:id])
+    @category_products_menu = CategoryProduct.where(show_menu: true)
+    @products = @category_product.products
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
   def show
+    @product = Product.find(params[:id])
+    @category_products_menu = CategoryProduct.where(show_menu: true)
   end
 end
