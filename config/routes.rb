a992fd61895e2195746ab0c89a73d8084ab4175b
Dmitriy Kovalev <rouxx26@gmail.com>
2017-01-24 20:31:51 +0300

    added pages /delivery /our-works-list /our-works-main

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   get 'home/index'
   resources :products, only:[:show]
   resources :category_products, only:[:show]
+
+  #временные страницы
+  get '/our-works-list', to: 'home#our-works-list'
+  get '/our-works-main', to: 'home#our-works-main'
+  get '/delivery', to: 'home#delivery'
 end
