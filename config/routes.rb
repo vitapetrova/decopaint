56f9c99cf16c0ccd7ecc09522a2d4face7e8c20c
Dmitriy Kovalev <rouxx26@gmail.com>
2017-01-25 15:29:08 +0300

    added to-designers, fixed left-menu, buttons

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
 
   #временные страницы
   get '/delivery', to: 'home#delivery'
+  get '/to-designers', to: 'home#to-designers'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,22 +30,6 @@ ActiveRecord::Schema.define(version: 20170124223809) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "ckeditor_assets", force: :cascade do |t|
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.string   "data_fingerprint"
-    t.integer  "assetable_id"
-    t.string   "assetable_type",    limit: 30
-    t.string   "type",              limit: 30
-    t.integer  "width"
-    t.integer  "height"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.index ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
-    t.index ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
-  end
-
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.float    "price"
