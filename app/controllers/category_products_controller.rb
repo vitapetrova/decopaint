15702f67aa7484cbe4479626b869717f38136342
shrewhools <vladyukha@yandex.ru>
2017-02-10 02:40:59 +0300

    priority category product

--- app/controllers/category_products_controller.rb
+++ app/controllers/category_products_controller.rb
@@ -1,7 +1,7 @@
 class CategoryProductsController < ApplicationController
   def show
     @category_product = CategoryProduct.find(params[:id])
-    @category_products_menu = CategoryProduct.where(show_menu: true)
+    @category_products_menu = CategoryProduct.where(show_menu: true).order(:priority)
     @products = @category_product.products
   end
 end
